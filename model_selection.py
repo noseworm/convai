aa6cbfd1be101ccc95c893747316b6e6f2b9c124
Koustuv Sinha <koustuv.sinha@mail.mcgill.ca>
2017-07-19 16:53:43 -0400

    warmup models before serving

--- model_selection.py
+++ model_selection.py
@@ -17,6 +17,10 @@ class ModelSelection(object):
         self.hred_model_twitter = HRED_Wrapper(conf.hred['twitter_model_prefix'], conf.hred['twitter_dict_file'], 'hred-twitter')
         self.hred_model_reddit = HRED_Wrapper(conf.hred['reddit_model_prefix'], conf.hred['reddit_dict_file'], 'hred-reddit')
         self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
+        # warmup the models before serving
+        r,_ = self.hred_model_twitter.get_response(1,'test statement',[])
+        r,_ = self.hred_model_reddit.get_response(1,'test statement',[])
+        r,_ = self.de_model_reddit.get_response(1,'test statement',[])
 
     def get_response(self,chat_id,text,context):
         # if text containes /start, dont add it to the context
@@ -32,11 +36,15 @@ class ModelSelection(object):
         resp1,cont1 = self.hred_model_twitter.get_response(chat_id,text,origin_context)
         origin_context = copy.deepcopy(context)
         resp2,cont2 = self.hred_model_reddit.get_response(chat_id,text,origin_context)
-        # doing a random toss for now
-        if random.choice([True,False]):
-            return resp1,cont1
-        else:
-            return resp2,cont2
+        origin_context = copy.deepcopy(context)
+        resp3,cont3 = self.de_model_reddit.get_response(chat_id,text,origin_context)
+ 
+	# doing a random toss for now
+        #if random.choice([True,False]):
+        #    return resp1,cont1
+        #else:
+        #    return resp2,cont2
+	return resp3,cont3
 
 
 
