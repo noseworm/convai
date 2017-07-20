0ddb7293e129e2632a184e862fa8d066f1bc4e9a
Koustuv Sinha <koustuv.sinha@mail.mcgill.ca>
2017-07-20 12:58:58 -0400

    update model selection logic

--- model_selection.py
+++ model_selection.py
@@ -17,12 +17,12 @@ class ModelSelection(object):
     def initialize_models(self):
         self.hred_model_twitter = HRED_Wrapper(conf.hred['twitter_model_prefix'], conf.hred['twitter_dict_file'], 'hred-twitter')
         self.hred_model_reddit = HRED_Wrapper(conf.hred['reddit_model_prefix'], conf.hred['reddit_dict_file'], 'hred-reddit')
-        #self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
+        self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
         self.qa_hred = HREDQA_Wrapper(conf.followup['model_prefix'],conf.followup['dict_file'],'followup_qa')
         # warmup the models before serving
         r,_ = self.hred_model_twitter.get_response(1,'test statement',[])
         r,_ = self.hred_model_reddit.get_response(1,'test statement',[])
-        #r,_ = self.de_model_reddit.get_response(1,'test statement',[])
+        r,_ = self.de_model_reddit.get_response(1,'test statement',[])
         r,_ = self.qa_hred.get_response(1,'test statement',[])
 
     def get_response(self,chat_id,text,context):
@@ -38,6 +38,9 @@ class ModelSelection(object):
             else:
                 resp = ''
             return resp,context
+        # chat selection logic
+        # if text contains a question, do not respond with a question (followup)
+
         outputs = []
         origin_context = copy.deepcopy(context)
         resp1,cont1 = self.hred_model_twitter.get_response(chat_id,text,origin_context)
@@ -49,12 +52,13 @@ class ModelSelection(object):
         resp3,cont3 = self.de_model_reddit.get_response(chat_id,text,origin_context)
         outputs.append((resp3,cont3))
         origin_context = copy.deepcopy(context)
-        resp4,cont4 = self.qa_hred.get_response(chat_id,text,origin_context)
-        outputs.append((resp4,cont4))
+        if '?' not in text:
+            resp4,cont4 = self.qa_hred.get_response(chat_id,text,origin_context)
+            outputs.append((resp4,cont4))
 
         # chat selection logic
         # for now, select in random
-        ch = random.choice([0,1,2,3])
+        ch = random.choice(range(len(outputs)))
 
 	return outputs[ch]
 
