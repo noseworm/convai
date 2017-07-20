cd3be1f6bc3ae85ae4698c027fe4636045a03302
Koustuv Sinha <koustuv.sinha@mail.mcgill.ca>
2017-07-19 20:50:21 -0400

    adjust model selection strategy

--- model_selection.py
+++ model_selection.py
@@ -12,44 +12,51 @@ class ModelSelection(object):
     def __init__(self):
         self.hred_model_twitter = None
         self.hred_model_reddit = None
+        self.article_text = {}
 
     def initialize_models(self):
         self.hred_model_twitter = HRED_Wrapper(conf.hred['twitter_model_prefix'], conf.hred['twitter_dict_file'], 'hred-twitter')
         self.hred_model_reddit = HRED_Wrapper(conf.hred['reddit_model_prefix'], conf.hred['reddit_dict_file'], 'hred-reddit')
-        self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
+        #self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
         self.qa_hred = HREDQA_Wrapper(conf.followup['model_prefix'],conf.followup['dict_file'],'followup_qa')
         # warmup the models before serving
         r,_ = self.hred_model_twitter.get_response(1,'test statement',[])
         r,_ = self.hred_model_reddit.get_response(1,'test statement',[])
-        r,_ = self.de_model_reddit.get_response(1,'test statement',[])
+        #r,_ = self.de_model_reddit.get_response(1,'test statement',[])
         r,_ = self.qa_hred.get_response(1,'test statement',[])
 
     def get_response(self,chat_id,text,context):
         # if text containes /start, dont add it to the context
         if '/start' in text:
-            # TODO: handle the article text!!
-            # - process it in the wrappers and store it for that `chat_id`
-
+            # save the article for later use
+            self.article_text[chat_id] = text
             # generate first response or not?
-            resp = 'Nice article, what is it about?'
-            context.append('<first_speaker>' + resp + '</s>')
+            # with some randomness generate the first response or leave blank
+            if random.choice([True,False]):
+                resp = 'Nice article, what is it about?'
+                context.append('<first_speaker>' + resp + '</s>')
+            else:
+                resp = ''
             return resp,context
+        outputs = []
         origin_context = copy.deepcopy(context)
         resp1,cont1 = self.hred_model_twitter.get_response(chat_id,text,origin_context)
+        outputs.append((resp1,cont1))
         origin_context = copy.deepcopy(context)
         resp2,cont2 = self.hred_model_reddit.get_response(chat_id,text,origin_context)
+        outputs.append((resp2,cont2))
         origin_context = copy.deepcopy(context)
         resp3,cont3 = self.de_model_reddit.get_response(chat_id,text,origin_context)
+        outputs.append((resp3,cont3))
         origin_context = copy.deepcopy(context)
         resp4,cont4 = self.qa_hred.get_response(chat_id,text,origin_context)
+        outputs.append((resp4,cont4))
 
+        # chat selection logic
+        # for now, select in random
+        ch = random.choice([0,1,2,3])
 
-	# doing a random toss for now
-        #if random.choice([True,False]):
-        #    return resp1,cont1
-        #else:
-        #    return resp2,cont2
-	return resp4,cont4
+	return outputs[ch]
 
 
 
