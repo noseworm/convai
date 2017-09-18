8310ba70bccf49d03d4d9f54eee235def42f88ac
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-09-18 16:18:50 -0400

    fixed indentation and comments. Added different exploratory policies

--- model_selection.py
+++ model_selection.py
@@ -13,6 +13,20 @@ import emoji
 
 nlp = spacy.load('en')
 
+
+class Policy:
+    NONE = -1        # between each chat
+    OPTIMAL = 1      # current set of rules
+    # exploratory policies:
+    HREDx2 = 2       # hred-reddit:0.5 & hred-twitter:0.5
+    HREDx3 = 3       # hred-reddit:0.33 & hred-twitter:0.33 & hred-qa:0.33
+    HREDx2_DE = 4    # hred-reddit:0.25 & hred-twitter:0.25 & DualEncoder:0.5
+    HREDx2_DRQA = 5  # hred-reddit:0.25 & hred-twitter:0.25 & DrQA:0.5
+    DE_DRQA = 6      # DualEncoder:0.5 & DrQA:0.5
+
+
+ALL_POLICIES = [Policy.OPTIMAL, Policy.HREDx2, Policy.HREDx3, Policy.HREDx2_DE, Policy.HREDx2_DRQA, Policy.DE_DRQA]
+
 BORED_COUNT = 2
 
 
@@ -23,44 +37,52 @@ class ModelSelection(object):
         self.candidate_model = {}  # map from chat_id to a simple model for the article
         self.article_nouns = {}    # map from chat_id to a list of nouns in the article
         self.boring_count = {}     # number of times the user responded with short answer
+        self.policy_mode = Policy.NONE
 
     def initialize_models(self):
         self.hred_model_twitter = HRED_Wrapper(conf.hred['twitter_model_prefix'], conf.hred['twitter_dict_file'], 'hred-twitter')
         self.hred_model_reddit = HRED_Wrapper(conf.hred['reddit_model_prefix'], conf.hred['reddit_dict_file'], 'hred-reddit')
         self.de_model_reddit = Dual_Encoder_Wrapper(conf.de['reddit_model_prefix'], conf.de['reddit_data_file'], conf.de['reddit_dict_file'], 'de-reddit')
-        self.qa_hred = HREDQA_Wrapper(conf.followup['model_prefix'],conf.followup['dict_file'],'followup_qa')
-        self.dumb_qa = DumbQuestions_Wrapper('',conf.dumb['dict_file'],'dumb_qa')
-        self.drqa = DRQA_Wrapper('','','drqa')
+        self.qa_hred = HREDQA_Wrapper(conf.followup['model_prefix'], conf.followup['dict_file'], 'followup_qa')
+        self.dumb_qa = DumbQuestions_Wrapper('', conf.dumb['dict_file'], 'dumb_qa')
+        self.drqa = DRQA_Wrapper('', '', 'drqa')
+
         # warmup the models before serving
-        r,_ = self.hred_model_twitter.get_response(1, 'test statement', [])
-        r,_ = self.hred_model_reddit.get_response(1, 'test statement', [])
-        r,_ = self.de_model_reddit.get_response(1, 'test statement', [])
-        r,_ = self.qa_hred.get_response(1, 'test statement', [])
-        r,_ = self.drqa.get_response(1, 'Where is Daniel?', [], 'Daniel went to the kitchen')
+        r, _ = self.hred_model_twitter.get_response(1, 'test statement', [])
+        r, _ = self.hred_model_reddit.get_response(1, 'test statement', [])
+        r, _ = self.qa_hred.get_response(1, 'test statement', [])
+        r, _ = self.de_model_reddit.get_response(1, 'test statement', [])
+        r, _ = self.drqa.get_response(1, 'Where is Daniel?', [], 'Daniel went to the kitchen')
+
 
     def clean(self, chat_id):
         del self.article_text[chat_id]
         del self.candidate_model[chat_id]
         del self.article_nouns[chat_id]
         del self.boring_count[chat_id]
+        self.policy_mode = Policy.NONE
 
     # get all the nouns from the text
     def _get_nouns(self, chat_id):
         self.article_nouns[chat_id] = [p.text for p in self.article_text[chat_id] if p.pos_ == 'NOUN']
         print self.article_nouns[chat_id]
 
-    def strip_emojis(self,str):
+    def strip_emojis(self, str):
         tokens = set(list(str))
         emojis = list(tokens.intersection(set(emoji.UNICODE_EMOJI.keys())))
         if len(emojis) > 0:
             text = ''.join(c for c in str if c not in emojis)
             emojis = ''.join(emojis)
-            return text,emojis
-        return str,None
+            return text, emojis
+        return str, None
 
     def get_response(self, chat_id, text, context):
-        # if text containes /start, dont add it to the context
+        # if text contains /start, don't add it to the context
         if '/start' in text:
+            # Make sure we didn't sample a policy before
+            assert self.policy_mode == Policy.NONE
+            self.policy_mode = random.choice(ALL_POLICIES)  # sample a random policy
+
             # save the article for later use
             text = re.sub('\\start','',text)
             self.article_text[chat_id] = nlp(unicode(text))
@@ -68,71 +90,177 @@ class ModelSelection(object):
             self.candidate_model[chat_id] = CandidateQuestions_Wrapper('', self.article_text[chat_id],
                                                                        conf.candidate['dict_file'], 'candidate_question')
             self.boring_count[chat_id] = 0  # initialize bored count to 0 for this new chat
-            # Always generate first response
-            #resp = 'Nice article, what is it about?'
+
             # add a small delay
             time.sleep(2)
+
             resp, context = self.candidate_model[chat_id].get_response(chat_id, '', context)
             if resp == '':
                 resp = random.choice(["That's a short article, don't you think? Not sure what's it about.","Apparently I am too dumb for this article. What's it about?"])
                 context.append('<first_speaker>' + resp + '</s>')
-            return resp,context
-        # chat selection logic
+            return resp, context
+
         # if text contains emoji's, strip them
-        text,emojis = self.strip_emojis(text)
+        text, emojis = self.strip_emojis(text)
         if emojis and len(text.strip()) < 1:
-            # give back the emoji itself
-            return emojis,context
- 
-        # if text contains a question, do not respond with a question (followup)
+            # if text had only emoji, give back the emoji itself
+            return emojis, context
+
         # if query falls under dumb questions, respond appropriately
         if self.dumb_qa.isMatch(text):
-            resp,context = self.dumb_qa.get_response(chat_id,text,context)
-            return resp,context
-       
-        # if text does not contain anything else
-        # if text contains question, run DRQA
+            resp, context = self.dumb_qa.get_response(chat_id, text, context)
+            return resp, context
+
+        ###
+        # chat selection logic
+        ###
+        assert self.policy_mode != Policy.NONE
+
+        if self.policy_mode == Policy.OPTIMAL:
+            return self.optimal_policy(chat_id, text, context)
+        elif self.policy_mode == Policy.HREDx2:
+            return self.hredx2_policy(chat_id, text, context)
+        elif self.policy_mode == Policy.HREDx3:
+            return self.hredx3_policy(chat_id, text, context)
+        elif self.policy_mode == Policy.HREDx2_DE:
+            return self.hredx2_de_policy(chat_id, text, context)
+        elif self.policy_mode == Policy.HREDx2_DRQA:
+            return self.hredx2_drqa_policy(chat_id, text, context)
+        elif self.policy_mode == Policy.DE_DRQA:
+            return self.de_drqa_policy(chat_id, text, context)
+        else:
+            print "ERROR: unknown policy mode:", self.policy_mode
+            return None, None
+
+    def optimal_policy(self, chat_id, text, context):
+        # if text contains question,
         if '?' in text:
-	    # if there is a common noun between text and article, run drqa
-	    common = list(set(self.article_nouns[chat_id]).intersection(set(text.split(' '))))
-            print 'common',common
-	    if len(common) > 0:
-            	resp,context = self.drqa.get_response(chat_id,text,context,article=self.article_text[chat_id].text)
-	    	return resp,context
-	    else:
-		if random.choice([True,False,False,False]): # sampling with 0.25 probability
-		   resp,context = self.drqa.get_response(chat_id,text,context,article=self.article_text[chat_id].text)
-		   return resp,context
+            # get list of common nouns between article and question
+            common = list(set(self.article_nouns[chat_id]).intersection(set(text.split(' '))))
+            print 'common nouns between question and article:', common
+            # if there is a common noun between question and article, run DrQA
+            if len(common) > 0:
+                resp, context = self.drqa.get_response(chat_id, text, context, article=self.article_text[chat_id].text)
+                return resp, context
 
         # if text contains 2 words or less, add 1 to the bored count
         if len(text.strip().split()) <= 2:
             self.boring_count[chat_id] += 1
         # if user is bored, change the topic by asking a question (only if that question is not asked before)
         if self.boring_count[chat_id] >= BORED_COUNT:
-            resp_c,context_c = self.candidate_model[chat_id].get_response(chat_id,'',copy.deepcopy(context))
+            resp_c, context_c = self.candidate_model[chat_id].get_response(chat_id, '', copy.deepcopy(context))
             if resp_c != '':
                 self.boring_count[chat_id] = 0  # reset bored count to 0
-                return resp_c,context_c
+                return resp_c, context_c
 
-        outputs = []
-        # randomly decide a model to generate. now we change the selection so that we pre-select the model to generate before hand.
-        models = ['hred-twitter','hred-reddit','de']
-        if '?' not in text:
-           models.append('qa')
+        # randomly decide a model to query to get a response:
+        models = ['hred-twitter', 'hred-reddit', 'de']
+        if '?' in text:
+            models.append('drqa')  # if the user asked a question, also consider DrQA
+        else:
+            models.append('qa')  # if the user didn't ask a question, also consider hred-qa
+
         chosen_model = random.choice(models)
         origin_context = copy.deepcopy(context)
         if chosen_model == 'hred-twitter':
-            resp,cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
-        if chosen_model == 'hred-reddit':
-            resp,cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+            resp, cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'hred-reddit':
+            resp, cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'de':
+            resp, cont = self.de_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'qa':
+            resp, cont = self.qa_hred.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'drqa':
+            resp, cont = self.drqa.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
+
+        return resp, cont
+
+    def hredx2_policy(self, chat_id, text, context):
+        # randomly decide a model to query to get a response:
+        models = ['hred-twitter', 'hred-reddit']
+        chosen_model = random.choice(models)
+        origin_context = copy.deepcopy(context)
+
+        if chosen_model == 'hred-twitter':
+            resp, cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'hred-reddit':
+            resp, cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
+
+        return resp, cont
+
+    def hredx3_policy(self, chat_id, text, context):
+        # randomly decide a model to query to get a response:
+        models = ['hred-twitter', 'hred-reddit', 'qa']
+        chosen_model = random.choice(models)
+        origin_context = copy.deepcopy(context)
+
+        if chosen_model == 'hred-twitter':
+            resp, cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+        elif chosen_model == 'hred-reddit':
+            resp, cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+        elif chosen_model == 'qa':
+            resp, cont = self.qa_hred.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
+
+        return resp, cont
+
+    def hredx2_de_policy(self, chat_id, text, context):
+        # randomly decide a model to query to get a response:
+        models = ['hred-twitter', 'hred-reddit', 'de', 'de']  # DE has probability .5, hred-reddit .25, hred-twitter .25
+        chosen_model = random.choice(models)
+        origin_context = copy.deepcopy(context)
+
+        if chosen_model == 'hred-twitter':
+            resp, cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+        elif chosen_model == 'hred-reddit':
+            resp, cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+        elif chosen_model == 'de':
+            resp, cont = self.de_model_reddit.get_response(chat_id, text, origin_context, self.article_text[chat_id])
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
+
+        return resp, cont
+
+    def hredx2_drqa_policy(self, chat_id, text, context):
+        # randomly decide a model to query to get a response:
+        models = ['hred-twitter', 'hred-reddit', 'drda', 'drqa']  # DrQA has probability .5, hred-reddit .25, hred-twitter .25
+        chosen_model = random.choice(models)
+        origin_context = copy.deepcopy(context)
+
+        if chosen_model == 'hred-twitter':
+            resp, cont = self.hred_model_twitter.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'hred-reddit':
+            resp, cont = self.hred_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'drqa':
+            resp, cont = self.drqa.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
+
+        return resp, cont
+
+    def de_drqa_policy(self, chat_id, text, context):
+        # randomly decide a model to query to get a response:
+        models = ['de', 'drqa']
+        chosen_model = random.choice(models)
+        origin_context = copy.deepcopy(context)
+
         if chosen_model == 'de':
-            resp,cont = self.de_model_reddit.get_response(chat_id, text, origin_context, self.article_text[chat_id])
-        if chosen_model == 'qa':
-            resp,cont = self.qa_hred.get_response(chat_id, text, origin_context, self.article_text.get(chat_id,''))
+            resp, cont = self.de_model_reddit.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        elif chosen_model == 'drqa':
+            resp, cont = self.drqa.get_response(chat_id, text, origin_context, self.article_text.get(chat_id, ''))
+        else:
+            print "ERROR: unknown chosen model:", chosen_model
+            return None, None
 
-        # chat selection logic
-        # for now, select in random
-        # ch = random.choice(range(len(outputs)))
-
-        return resp,cont
+        return resp, cont
 
