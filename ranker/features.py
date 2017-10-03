eef226b108684d4da204adeda585974f1df45eef
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-10-03 11:11:05 -0400

    updated ranker features code design

--- ranker/features.py
+++ ranker/features.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 """
 This file defines different hand-enginered features based
@@ -9,20 +8,6 @@ fully-connected feed-foward neural network to predict either
 the full dialogue score or the utterance score.
 """
 
-def dimension(feature_list):
-    """
-    Return the number of dimensiona a given set of features will be
-    :feature_list: list of features to consider.
-    :return: an integer for the dimension of the features.
-    """
-    feature_list = [f.lower() for f in feature_list]
-
-    dim = 0
-    if '<candidate_length>' in feature_list or '<all>' in feature_list:
-        dim += 1
-    elif '<user_length>' in feature_list or '<all>' in feature_list:
-        dim += 1
-
 
 def get(article, context, candidate, feature_list):
     """
@@ -32,34 +17,68 @@ def get(article, context, candidate, feature_list):
     :param context: the list of user & bot utterances so far
     :param candidate: the candidate response one model proposed
     :feature_list: list of features to return for the above triple.
-    :return: a numpy aray containing all features you requested for.
+    :return: an aray containing all feature objects you requested for.
     """
-    feature_list = [f.lower() for f in feature_list]
+    feature_objects = []  # list of feature objects
 
-    features = np.array([])
+    for f in feature_list:
+        feature = eval(f)(article, context, candidate)
+        feature_objects.append(feature)
+        
 
-    if '<candidate_length>' in feature_list or '<all>' in feature_list:
-        features.append(_candidate_length(candidate))
-    elif '<user_length>' in feature_list or '<all>' in feature_list:
-        features.append(_user_length(context))
-    # TODO: continue defining new features like embedding metrics, word overlap metrics, lookup for specific words, etc...
-    else:
-        print "WARNING: no feature recognized in %s" % feature_name
+    if len(feature_objects) == 0:
+        print "WARNING: no feature recognized in %s" % (feature_list,)
     
-    return features
+    return feature_objects
 
 
-def _candidate_length(candidate):
-    """
-    :return: float scalar (dim: 1) for the length of the candidate response
-    """
-    return float(len(candidate))
+class Feature(Object):
 
-def _user_length(context):
-    """
-    :return: float scalar (dim: 1) for the length of the previous user utterance
-    """
-    return float(len(context[-1]))
+    self.dim = -1
+    self.feat = None
+
+    def __init__(self, dim, article=None, context=None, candidate=None):
+        self.dim = dim
+        
+    def set(self, article, context, candidate):
+        """
+        To be implemented in each sub-class
+        """
+        pass
+
+
+class CandidateLength(Feature):
+
+    def __init__(self, article=None, context=None, candidate=None):
+        # Constructor: call super class constructor with dim=1
+        super(CandidateLength, self).__init__(1, article, context, candidate)
+        self.set(article, context, candidate)
+
+    def set(self, article, context, candidate):
+        """
+        set feature attribure to float scalar (dim: 1) for the length of the candidate response
+        """
+        if candidate is None:
+            self.feat = None
+        else:
+            self.feat = float(len(candidate))
+
+
+class UserLength(Feature):
+
+    def __init__(self, article=None, context=None, candidate=None):
+        # Constructor: call super class constructor with dim=1
+        super(UserLength, self).__init__(1, article, context, candidate)
+        self.set(article, context, candidate)
+
+    def set(self, article, context, candidate):
+        """
+        set feature attribute to float scalar (dim: 1) for the length of the previous user utterance
+        """
+        if context = None:
+            self.feat = None
+        else:
+            self.feat = float(len(context[-1]))
 
 
 # TODO: continue defining new features like embedding metrics, word overlap metrics, lookup for specific words, etc...
