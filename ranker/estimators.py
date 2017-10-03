5f72663505f4480eedc3fa444416d2d521978c1a
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-10-03 01:14:07 -0400

    ranker second thoughts

--- ranker/estimators.py
+++ ranker/estimators.py
@@ -7,11 +7,13 @@ reward signals: both short-term signal (bot response being up- or down- voted)
 and long term signal (average conversation score between 1 and 5).
 """
 
-class ShortEsdtimator1(Object):
-    # TODO (Nico) fully connected feed forward neural net using tensorflow 
+class ShortEstimator1(Object):
+    self.features_needed = "<all>"  # probably not all of them
+    # TODO (Nico) fully connected feed forward neural net using tensorflow
 
 
 class LongEstimator1(Object):
+    self.features_needed = "<all>"  # probably not all of them...
     # TODO (Nico) fully connected feed forward neural net using tensorflow
 
 
--- ranker/features.py
+++ ranker/features.py
@@ -9,24 +9,38 @@ fully-connected feed-foward neural network to predict either
 the full dialogue score or the utterance score.
 """
 
-def get(article, context, candidate, feature_name):
+def dimension(feature_list):
+    """
+    Return the number of dimensiona a given set of features will be
+    :feature_list: list of features to consider.
+    :return: an integer for the dimension of the features.
+    """
+    feature_list = [f.lower() for f in feature_list]
+
+    dim = 0
+    if '<candidate_length>' in feature_list or '<all>' in feature_list:
+        dim += 1
+    elif '<user_length>' in feature_list or '<all>' in feature_list:
+        dim += 1
+
+
+def get(article, context, candidate, feature_list):
     """
     The only method to call to get all features we want for a triple
     of (article, context, candidate response).
     :param article: the text of the conversation article to talk to
     :param context: the list of user & bot utterances so far
     :param candidate: the candidate response one model proposed
-    :feature_name: string of features to return for the above triple.
-        Note: all feature names should be between brackets ('<' and '>').
+    :feature_list: list of features to return for the above triple.
     :return: a numpy aray containing all features you requested for.
     """
-    feature_name = feature_name.lower()
+    feature_list = [f.lower() for f in feature_list]
 
     features = np.array([])
 
-    if '<candidate_length>' in feature_name or '<all>' in feature_name:
+    if '<candidate_length>' in feature_list or '<all>' in feature_list:
         features.append(_candidate_length(candidate))
-    elif '<user_length>' in feature_name or '<all>' in feature_name:
+    elif '<user_length>' in feature_list or '<all>' in feature_list:
         features.append(_user_length(context))
     # TODO: continue defining new features like embedding metrics, word overlap metrics, lookup for specific words, etc...
     else:
