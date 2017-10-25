ed67132399225e13e2db22296c88100e14c33382
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-10-24 22:18:07 -0400

    implemented ranker neural net with low level tf API rather than complicated software dev highlevel APIs Grrrrr

--- ranker/train_estimators.py
+++ ranker/train_estimators.py
@@ -7,13 +7,10 @@ import pyprind
 import random
 import copy
 import sys
+import os
 
 import features
 
-
-FLAGS = None
-
-tf.logging.set_verbosity(tf.logging.INFO)
 
 TARGET_TO_FEATURES = {
     'r': ['AverageWordEmbedding_Candidate', 'AverageWordEmbedding_User', 'AverageWordEmbedding_Article',
@@ -29,274 +26,148 @@ TARGET_TO_FEATURES = {
 }
 
 
-def get_data(target, voted_only=False, test_prop=0.2):
+# def weight_variable(name, shape, mean=0., stddev=0.1):
+#     """
+#     Build a tensorflow variable oject initialized with the normal distribution
+#     :param name: name of the variable
+#     :param shape: shape of the variable
+#     :param mean: the mean of the normal distribution to sample from
+#     :param stddev: the standard deviation of the normal distribution to sample from
+#     :return: tf.Variable() object to update during training
+#     """
+#     initial = tf.truncated_normal(shape, mean=mean, stddev=stddev, name=name)
+#     return tf.Variable(initial)
+
+
+# def bias_variable(name, shape, cst=0.1):
+#     """
+#     Build a tensorflow variable oject initialized to some value
+#     :param name: name of the variable
+#     :param shape: shape of the variable
+#     :param cst: the initial value of the variable
+#     :return: tf.Variable() object to update during training
+#     """
+#     initial = tf.constant(cst, shape=shape, name=name)
+#     return tf.Variable(initial)
+
+
+def build_model1(input_dim, hidden_dims, activation, optimizer):
     """
-    Load data to train ranker
-    :param target: field of each dictionary instance to estimate
-        either 'R' for final dialog score, or 'r' for immediate reward
-    :param voted_only: load messages which have been voted only
-    :param test_prop: proportion of data to consider for test set
-    :return: input size (int), train x & y, test x & y
-        x = map from feature_name to numpy array of size (data, feature_length)
-          ie: { <name of feature 1>: np.array( [[f1_dim],...,[f1_dim]] ),
-                <name of feature 2>: np.array( [[f2_dim],...,[f2_dim]] ),
-                ... }
-        y = array of label values to predict
-        input_size = feat_1.dim + ... + feat_n.dim
+    Build the model to estimate immediate reward: +1 or -1
+    :param input_dim: the number of neurons in the input layer
+    :param hidden_dims: list of ints specifying the size of each hidden layer
+    :param activation: tensor activation function to use at each layer
+    :param optimizer: tensorflow optimizer object to train the network
+    :return: predictions (dict of predicted classes and probabilities),
+             loss, training operator, and accuracy
     """
-    assert target in ['R', 'r'], "Unknown target: %s" % target
 
-    print "\nLoading data..."
-    data = []
-    for data_file in FLAGS.data:
-        if voted_only and data_file.startswith('voted_data_'):
-            with open(data_file, 'rb') as handle:
-                data.extend(pkl.load(handle))
-        elif (not voted_only) and data_file.startswith('full_data_'):
-            with open(DATA_DB_FULL, 'rb') as handle:
-                data.extend(pkl.load(handle))
-    print "got %d examples" % len(data)
+    x = tf.placeholder(tf.float32, shape=[None, input_dim], name="input_layer")  # (bs, feat_size)
+    y = tf.placeholder(tf.float32, shape=[None, 1], name="labels")  # (bs, 1)
 
-    # shuffle data
-    random.shuffle(data)
-
-    idx = int(len(data) * (1-test_prop))  # idx to start test data
-    train_data = data[:idx]
-    test_data = data[idx:]
-    print "train: %d" % len(train_data)
-    print "test: %d" % len(test_data)
-
-    # create list of Feature instances
-    feature_objects = features.get(None, None, None, TARGET_TO_FEATURES[target])
-
-    # construct data to return
-    input_size = np.sum([f.dim for f in feature_objects])
-    train_x = dict(
-        [(f.__class__.__name__, np.zeros((len(train_data), f.dim))) for f in feature_objects]
-    )
-    train_y = []
-    test_x = dict(
-        [(f.__class__.__name__, np.zeros((len(test_data), f.dim))) for f in feature_objects]
-    )
-    test_y = []
-
-    print "building data..."
-    bar = pyprind.ProgBar(len(train_data)+len(test_data), monitor=False, stream=sys.stdout)  # show a progression bar on the screen
-    for (x, y, data) in [(train_x, train_y, train_data), (test_x, test_y, test_data)]:
-        for idx, msg in enumerate(data):
-            # set x for each feature for that msg
-            for f in feature_objects:
-                f.set(msg['article'], msg['context'], msg['candidate']) 
-                x[f.__class__.__name__][idx, :] = np.array(copy.deepcopy(f.feat), dtype=np.float32)
-            # set y labels
-            if target == 'r':
-                if int(msg[target]) == -1:  y.append(0)
-                elif int(msg[target]) == 1: y.append(1)
-                else: print "ERROR: unknown immediate reward value: %s" % msg[target]
-            else:
-                y.append(msg[target])
-            bar.update()
-    print "done."
-
-    for f in feature_objects:
-        assert train_x[f.__class__.__name__].shape[0] == len(train_y)
-        assert test_x[f.__class__.__name__].shape[0] == len(test_y)
-
-    return input_size, train_x, np.array(train_y), test_x, np.array(test_y)
-    
-
-
-def model1_fn(features, labels, mode, params):
-    """
-    # 1. Configure the model via TensorFlow operations
-    # 2. Define the loss function for training/evaluation
-    # 3. Define the training operation/optimizer
-    # 4. Generate predictions
-    # 5. Return predictions/loss/train_op/eval_metric_ops in EstimatorSpec object
-
-    :param features: A dict containing the features passed to the model via `input_fn`
-    :param labels: A Tensor containing the labels passed to the model via `input_fn` -- (bs,)
-        Will be empty for predict() calls, as these are the values the model will infer
-    :param model: indicating the context in which the model_fn was invoked:
-        tf.estimator.ModeKeys.TRAIN -- in training mode, namely via a `train()` call
-        tf.estimator.ModeKeys.EVAL  -- in evaluation mode, namely via an `evaluate()` call
-        tf.estimator.ModeKeys.PREDICT -- in predict mode, namely via a `predict()` call
-    :param params: additional parameters passed as a dictionary:
-        + 'input_size' --> int ; size of input layer
-        + 'hidden_sizes' --> array of ints ; size of each hidden layers
-        + 'activation' --> tf function (ex: tf.nn.swish, tf.nn.relu, tf.sigmoid, ...)
-        + 'drop_rate' --> float ; probability of dropping a dimension
-        + 'optimizer' --> tf Optimizer Object used for training (ex: tf.train.AdamOptimizer())
-
-    :return: a tf.estimator.EstimatorSpec object, which contains the following values:
-        + mode (required) -- mode in which the model was run
-        + predictions (required in PREDICT mode) -- dict that maps key names to prediction Tensors from the model
-        + loss (required in EVAL and TRAIN mode) -- Tensor containing model's loss function calculated over all input examples
-        + train_op (required only in TRAIN mode) -- Op that runs one step of training
-        + eval_metric_ops (optional) -- dict of name/value pairs specifying the metrics that will be calculated when the model runs in EVAL mode.
-    """
-    for p in ['input_size', 'hidden_sizes', 'activation', 'drop_rate', 'optimizer']:
-        assert p in params
-
-    print "input size: %d" % params['input_size']
-    input_layer = tf.concat(
-        features.values(),  # (bs, f_dim)
-        axis = 1,
-        name = "input_layer"
-    )
-    print "input_layer: %s" % input_layer
-
-    # Forward prediction:
-    # (1) dense layers with activation
-    dense = input_layer  # (bs, in)
-    for idx, hidd in enumerate(params['hidden_sizes']):
-        dense = tf.layers.dense(inputs     = dense,
-                                units      = hidd,
-                                activation = params['activation'],
-                                name       = 'dense_layer_%d' % idx)  # (bs, h_i)
-    # (2) dropout
-    if params['drop_rate'] > 0.0:
-        dense = tf.layers.dropout(inputs   = dense,
-                                  rate     = params['drop_rate'],
-                                  training = (mode == tf.estimator.ModeKeys.TRAIN),
-                                  name     = 'dropout_layer')  # (bs, h_last)
-    # (3) dense w/o activation
-    logits = tf.layers.dense(inputs = dense,
-                             units  = 2,  # binary classification: predict +1/-1
-                             # no activation: linear activation
+    # Fully connected dense layers
+    h_fc = x  # (bs, in)
+    for idx, hidd in enumerate(hidden_dims):
+        h_fc = tf.layers.dense(inputs     = h_fc,
+                               units      = hidd,
+                               activation = activation,
+                               name       = 'dense_layer_%d' % idx+1)  # (bs, hidd)
+    # Dropout layer
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")  # proba of keeping the neuron
+    h_fc_drop = tf.nn.dropout(h_fc, keep_prob)
+    # Output layer
+    logits = tf.layers.dense(inputs = h_fc_drop,
+                             units  = 2,
+                             # no activation for the logits
                              name   = 'logits_layer')  # (bs, 2)
 
-    # The logits layer of our model returns our predictions as raw values in a [batch_size, 2]-dimensional tensor.
-    # Let's convert these raw values into two different formats that our model function can return:
-    # - The predicted class for each example: either 1 or 0
-    # - The probabilities for each possible target class for each example
+    # Define prediction: class label (0,1) and the class probabilities:
     predictions = {
-        "classes"       : tf.argmax(logits, axis=1),  # (bs,)
-        "probabilities" : tf.nn.softmax(logits, name="softmax_tensor")  # (bs, 2)
+        "classes"       : tf.argmax(logits, axis=1, name="pred_classes"),  # (bs,)
+        "probabilities" : tf.nn.softmax(logits, name="pred_probas")  # (bs, 2)
     }
-    # RETURN HERE IN PREDICT MODE
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    # convert list of labels [0, 1, ..., 1] ~ (bs,) to one-hot encodings [[1,0], [0,1], ..., [0,1]] ~ (bs, 2)
-    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=2)  # (bs, 2)
+    # Loss tensor:
+    # create one-hot labels
+    onehot_labels = tf.one_hot(indices=tf.cast(y, tf.int32), depth=2)  # (bs, 2)
     # define the cross-entropy loss
     loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
 
-    # RETURN HERE IN TRAINING MDE
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        train_op = params['optimizer'].minimize(
-            loss        = loss,
-            global_step = tf.train.get_global_step()
-        )
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    # Train operator:
+    train_step = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
-    # RETURN HERE IN EVAL MODE
-    # add the accuracy metric:
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
-    }
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    # Accuracy tensor:
+    accuracy = tf.metrics.accuracy(labels=y, predictions=predictions["classes"], name="accuracy")
+    
+    return predictions, loss, train_step, accuracy
 
 
-def model2_fn():
-    pass
-
-
-def main(unused_argv):
-    """
-    tuto: https://www.tensorflow.org/tutorials/layers#evaluate_the_model
-    """
+def main(args):
     # Load datasets
-    input_size, x_train, y_train, x_test, y_test = get_data('r', voted_only=True)
+    input_size, (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data('r', voted_only=True)
 
     # parse arguments
-    if FLAGS.activation_1 == 'swish':
+    if args.activation_1 == 'swish':
         activation_1 = lambda x: x*tf.sigmoid(x)
-    elif FLAGS.activation_1 == 'relu':
+    elif args.activation_1 == 'relu':
         activation_1 = tf.nn.relu
-    elif FLAGS.activation_1 == 'sigmoid':
+    elif args.activation_1 == 'sigmoid':
         activation_1 = tf.sigmoid
     else:
-        print "ERROR: unknown activation 1: %s" % FLAGS.activation_1
+        print "ERROR: unknown activation 1: %s" % args.activation_1
         return
 
-    # Build first RNN to predict immediate reward: +1 / -1
-    with tf.device('/gpu:%d' % FLAGS.gpu):
-        nn1 = tf.estimator.Estimator(
-            model_fn  = model1_fn,
-            params    = {
-                'input_size'  : input_size,
-                'hidden_sizes': FLAGS.hidden_sizes_1,
-                'activation'  : activation_1,  # try tf.nn.swish | tf.nn.relu | tf.sigmoid
-                'drop_rate'   : FLAGS.dropout_rate_1,  # try 0.1 | 0.3 | 0.5 | 0.7 | 0.9
-                'optimizer'   : tf.train.AdamOptimizer()
-            },
-            model_dir = "./models/vote_estimator_model"
-        )
+    # TODO: save model when early stop
+    model1_dir = "./models/vote_estimator_model"
+    # TODO: use batch size that make sense according to data_size
+    steps = len(y_train) / FLAGS.batch_size
 
-    # Set up logging for predictions
-    # tensors_to_log = {  # key = string to be printed && value = name of tensor to print
-    #     "probabilities": "softmax_tensor"
-    # }
-    # log probabilities after every n steps of training
-    # logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=10)
-
-    # Define the training inputs: use tf.estimator.inputs.numpy_input_fn to produce the input pipeline
-    # Returns input function that would feed dict of numpy arrays into the model.
-    # This returns a function outputting features and target based on the dict of numpy arrays.
-    train_input_fn1 = tf.estimator.inputs.numpy_input_fn(
-        x          = x_train,           # map from 'feature_name' to numpy array ~ (data, feat_dim)
-        y          = y_train,           # list of labels (0 for downvote / 1 for upvote)
-        batch_size = FLAGS.batch_size,  # number of examples per training step
-        num_epochs = None,              # train until specified number of train steps is reached
-        shuffle    = True               # shuffle the training data
-    )
-    # Train the model
-    nn1.train(
-        input_fn = train_input_fn1,
-        steps    = FLAGS.train_steps  # train n steps (*batch_size = number of seen examples) 
-        # hooks    = [logging_hook]
+    predictions, loss, train_step, accuracy = build_model1(
+        input_size,
+        args.hidden_sizes_1,
+        activation_1,
+        tf.train.AdamOptimizer()
     )
 
-    # Evaluate the model and print results
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x          = x_test,
-        y          = y_test,
-        num_epochs = 1,  # evaluates the <F4>metrics over one epoch of data only
-        shuffle    = False  # iterate through the data sequentially
-    )
-    eval_results = nn1.evaluate(input_fn=eval_input_fn)  # return eval_metric_ops
-    print(eval_results)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())  # initialize model
+        for epoch in range(20000):  # will probably stop before 20k epochs due to early stop
+            batch = data.train.next_batch(args.batch_size)  # TODO: get data batch loader
+            while batch is not None:
+                # train 
+                train_step.run(
+                    feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0-args.dropout_rate_1}
+                )
+                # evaluate (so no dropout) train accuracy
+                train_acc = accuracy.eval(
+                    feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0}
+                )
+                print "epoch %d: train accuracy: %g" % (epoch, train_acc)
+                # get next training batch
+                batch = data.train.next_batch(args.batch_size)
+            # TODO: Evaluate on validation set and early stop & save!
 
-
-    ###
-    # SECOND PREDICTOR
-    ###
-    # nn2 = tf.estimator.Estimator(
-    #     model_fn  = model2_fn,
-    #     params    = {},
-    #     model_dir = "./models/finalscore_model"
-    # )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.register("type", "bool", lambda v: v.lower() == "true")
     parser.add_argument("data", nargs='+', type=str, help="List of files to consider for training")
     parser.add_argument("-g",  "--gpu", type=int, default=0, help="GPU number to use")
     # training parameters:
     parser.add_argument("-bs", "--batch_size", type=int, default=128, help="batch size during training")
-    parser.add_argument("-ts", "--train_steps", type=int, default=10000, help="number of batched seen during training")
+    parser.add_argument("-p", "--patience", type=int, default=10, help="Number of training steps to wait before stoping when validatiaon accuracy doesn't increase")
     # network architecture:
-    parser.add_argument("-h1", "--hidden_sizes_1", nargs='+', type=int, default=[500, 100, 10], help="List of hidden sizes for first network")
-    parser.add_argument("-h2", "--hidden_sizes_2", nargs='+', type=int, default=[500, 100, 10], help="List of hidden sizes for second network")
+    parser.add_argument("-h1", "--hidden_sizes_1", nargs='+', type=int, default=[500, 300, 100, 10], help="List of hidden sizes for first network")
+    parser.add_argument("-h2", "--hidden_sizes_2", nargs='+', type=int, default=[500, 300, 100, 10], help="List of hidden sizes for second network")
     parser.add_argument("-a1", "--activation_1", choices=['sigmoid', 'relu', 'swish'], type=str, default='relu', help="Activation function for first network")
     parser.add_argument("-a2", "--activation_2", choices=['sigmoid', 'relu', 'swish'], type=str, default='relu', help="Activation function for second network")
     parser.add_argument("-d1", "--dropout_rate_1", type=float, default=0.1, help="Probability of dropout layer in first network")
     parser.add_argument("-d2", "--dropout_rate_2", type=float, default=0.1, help="Probability of dropout layer in second network")
-    FLAGS, unparsed = parser.parse_known_args()
-    print '\n',FLAGS
+    args = parser.parse_args()
+    print args
 
-    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % args.gpu
+
+    main(args)
 
