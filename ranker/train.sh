a58b1f92a6397a68121830265343b8b50d17c9a5
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-11-01 19:07:03 -0400

    reward estimation is learning!

--- ranker/train.sh
+++ ranker/train.sh
@@ -1,11 +1,12 @@
+#!/usr/bin/env bash
+
 ########################################
 # Train estimator for immediate reward #
 ########################################
-python train_estimators.py voted_data_db_1508722816.49.pkl voted_data_round1_1508722837.51.pkl \
+python train_estimators.py voted_data_db_1509563020.37.pkl voted_data_round1_1509562999.05.pkl \
     --gpu 2 \
     -bs 128 \
-    -ts 10000 \
-    -h1 500 300 100 50 \
-    -a1 swish \
-    -d1 0.1
+    -h1 500 300 100 50 \
+    -a1 swish \
+    -d1 0.1
 
--- ranker/train_estimators.py
+++ ranker/train_estimators.py
@@ -35,11 +35,10 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
     :param voted_only: load messages which have been voted only
     :param val_prop: proportion of data to consider for validation set
     :param test_prop: proportion of data to consider for test set
-    :return: input size (int), train x & y, test x & y
+    :return: train x & y, valid x & y, test x & y
         x = numpy array of size (data, feature_length)
           ie: np.array( [[f1, f2, ..., fn], ..., [f1, f2, ..., fn]] )
         y = array of label values to predict
-        input_size = feat_1.dim + ... + feat_n.dim
     """
     assert target in ['R', 'r'], "Unknown target: %s" % target
 
@@ -57,6 +56,8 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
                 data.extend(pkl.load(handle))
                 # get the time id of the data
                 file_ids.append(data_file.split('_')[-1].replace('pkl', ''))
+        else:
+            print "Warning: will not consider file %s because voted_only=%s" % (data_file, voted_only)
 
     # if didn't get train/valid/test data, build your own
     if len(data) > 3:
@@ -75,7 +76,7 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
         print "test: %d" % len(test_data)
 
         # create list of Feature instances
-        feature_objects = features.get(None, None, None, TARGET_TO_FEATURES[target])
+        feature_objects = features.get(article=None, context=None, candidate=None, feature_list=TARGET_TO_FEATURES[target])
         input_size = np.sum([f.dim for f in feature_objects])
 
         # construct data to save & return
@@ -86,15 +87,17 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
         test_x = np.zeros((len(test_data), input_size))
         test_y = []
 
-        # TODO: continue corrections below!!
         print "building data..."
         bar = pyprind.ProgBar(len(data), monitor=False, stream=sys.stdout)  # show a progression bar on the screen
         for (x, y, data) in [(train_x, train_y, train_data), (valid_x, valid_y, valid_data), (test_x, test_y, test_data)]:
             for idx, msg in enumerate(data):
-                # set x for each feature for that msg
+                # create input features for this msg:
+                tmp = []
                 for f in feature_objects:
+                    # Set x for each feature for that msg
                     f.set(msg['article'], msg['context'], msg['candidate'])
-                    x[f.__class__.__name__][idx, :] = np.array(copy.deepcopy(f.feat), dtype=np.float32)
+                    tmp.extend(copy.deepcopy(f.feat))
+                x[idx, :] = np.array(tmp, dtype=np.float32)
                 # set y labels
                 if target == 'r':
                     if int(msg[target]) == -1:  y.append(0)
@@ -106,10 +109,6 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
         train_y = np.array(train_y)
         valid_y = np.array(valid_y)
         test_y = np.array(test_y)
-
-        for f in feature_objects:
-            assert train_x[f.__class__.__name__].shape[0] == len(train_y)
-            assert test_x[f.__class__.__name__].shape[0] == len(test_y)
 
         # save train/val/test data to pkl file
         file_name = ""
@@ -132,16 +131,17 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
     # got train/valid/test data
     elif len(data) == 3:
         (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = data
-        input_size = np.sum([feat.shape[1] for feat in train_x.values()])
-        print "train: %d" % len(train_y)
-        print "valid: %d" % len(valid_y)
-        print "test: %d" % len(test_y)
+        print "train: %s" % (train_x.shape,)
+        print "valid: %s" % (valid_x.shape,)
+        print "test: %s" % (test_x.shape,)
+        # make sure train, valid, test have the same amount of features.
+        assert train_x.shape[1] == valid_x.shape[1] == test_x.shape[1]
 
     else:
         print "Unknown data format: data length is less than 3."
         return
 
-    return input_size, (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
 
 
 # def weight_variable(name, shape, mean=0., stddev=0.1):
@@ -169,60 +169,134 @@ def get_data(files, target, voted_only=False, val_prop=0.1, test_prop=0.1):
 #     return tf.Variable(initial)
 
 
-def build_model1(input_dim, hidden_dims, activation, optimizer):
-    """
-    Build the model to estimate immediate reward: +1 or -1
-    :param input_dim: the number of neurons in the input layer
-    :param hidden_dims: list of ints specifying the size of each hidden layer
-    :param activation: tensor activation function to use at each layer
-    :param optimizer: tensorflow optimizer object to train the network
-    :return: predictions (dict of predicted classes and probabilities),
-             loss, training operator, and accuracy
-    """
+class ShortTermEstimator(object):
+    def __init__(self, data, hidden_dims, activation, optimizer):
+        """
+        Build the estimator for short term reward: +1 / -1
+        :param data: train, valid, test data to use
+        :param hidden_dims: list of ints specifying the size of each hidden layer
+        :param activation: tensor activation function to use at each layer
+        :param optimizer: tensorflow optimizer object to train the network
+        """
+        (self.x_train, self.y_train), (self.x_valid, self.y_valid), (self.x_test, self.y_test) = data
+        self.n, self.input_dim = self.x_train.shape
 
-    x = tf.placeholder(tf.float32, shape=[None, input_dim], name="input_layer")  # (bs, feat_size)
-    y = tf.placeholder(tf.float32, shape=[None, 1], name="labels")  # (bs, 1)
+        self.hidden_dims = hidden_dims
+        self.activation = activation
+        self.optimizer = optimizer
 
-    # Fully connected dense layers
-    h_fc = x  # (bs, in)
-    for idx, hidd in enumerate(hidden_dims):
-        h_fc = tf.layers.dense(inputs     = h_fc,
-                               units      = hidd,
-                               activation = activation,
-                               name       = 'dense_layer_%d' % idx+1)  # (bs, hidd)
-    # Dropout layer
-    keep_prob = tf.placeholder(tf.float32, name="keep_prob")  # proba of keeping the neuron
-    h_fc_drop = tf.nn.dropout(h_fc, keep_prob)
-    # Output layer
-    logits = tf.layers.dense(inputs = h_fc_drop,
-                             units  = 2,
-                             # no activation for the logits
-                             name   = 'logits_layer')  # (bs, 2)
+        self.build()
 
-    # Define prediction: class label (0,1) and the class probabilities:
-    predictions = {
-        "classes"       : tf.argmax(logits, axis=1, name="pred_classes"),  # (bs,)
-        "probabilities" : tf.nn.softmax(logits, name="pred_probas")  # (bs, 2)
-    }
+    def build(self):
+        """
+        Build the actual neural net, define the predictions, loss, train operator, and accuracy
+        """
+        self.x = tf.placeholder(tf.float32, shape=[None, self.input_dim], name="input_layer")  # (bs, feat_size)
+        self.y = tf.placeholder(tf.int64, shape=[None, ], name="labels")  # (bs,)
 
-    # Loss tensor:
-    # create one-hot labels
-    onehot_labels = tf.one_hot(indices=tf.cast(y, tf.int32), depth=2)  # (bs, 2)
-    # define the cross-entropy loss
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+        # Fully connected dense layers
+        h_fc = self.x  # (bs, in)
+        for idx, hidd in enumerate(self.hidden_dims):
+            h_fc = tf.layers.dense(inputs=h_fc,
+                                   units=hidd,
+                                   # kernel_initializer = Initializer function for the weight matrix.
+                                   # bias_initializer: Initializer function for the bias.
+                                   activation=self.activation,
+                                   name='dense_layer_%d' % (idx + 1))  # (bs, hidd)
+        # Dropout layer
+        self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")  # proba of keeping the neuron
+        h_fc_drop = tf.nn.dropout(h_fc, self.keep_prob)
+        # Output layer
+        self.logits = tf.layers.dense(inputs=h_fc_drop,
+                                      units=2,
+                                      # no activation for the logits
+                                      name='logits_layer')  # (bs, 2)
 
-    # Train operator:
-    train_step = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+        # Define prediction: class label (0,1) and the class probabilities:
+        self.predictions = {
+            "classes": tf.argmax(self.logits, axis=1, name="pred_classes"),  # (bs,)
+            "probabilities": tf.nn.softmax(self.logits, name="pred_probas")  # (bs, 2)
+        }
 
-    # Accuracy tensor:
-    accuracy = tf.metrics.accuracy(labels=y, predictions=predictions["classes"], name="accuracy")
-    
-    return predictions, loss, train_step, accuracy
+        # Loss tensor:
+        # create one-hot labels
+        onehot_labels = tf.one_hot(indices=tf.cast(self.y, tf.int32), depth=2)  # (bs, 2)
+        # define the cross-entropy loss
+        self.loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=self.logits)
+
+        # Train operator:
+        self.train_step = self.optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
+
+        # Accuracy tensor:
+        correct_predictions = tf.equal(
+            self.predictions['classes'], self.y
+        )
+        self.accuracy = tf.reduce_mean(
+            tf.cast(correct_predictions, tf.float32)
+        )
+        # self.accuracy, _ = tf.metrics.accuracy(labels=self.y, predictions=self.predictions["classes"], name="accuracy")
+
+    def train(self, patience, batch_size, dropout_rate):
+        self.train_accuracies = []
+        self.valid_accuracies = []
+
+        best_valid_acc = 0.0
+        p = patience
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())  # initialize model
+            for epoch in range(20000):  # will probably stop before 20k epochs due to early stop
+                # do 1 epoch: go through all training_batches
+                for idx in range(0, self.n, batch_size):
+                    _, loss = sess.run(
+                        [self.train_step, self.loss],
+                        feed_dict={self.x: self.x_train[idx: idx + batch_size],
+                                   self.y: self.y_train[idx: idx + batch_size],
+                                   self.keep_prob: 1.0 - dropout_rate}
+                    )
+                    step = idx / batch_size
+                    if step % 10 == 0:
+                        print "epoch %d - step %d - training loss: %g" % (epoch, step, loss)
+                print "epoch %d - step %d - training loss: %g" % (epoch, step, loss)
+                print "------------------------------"
+                # Evaluate (so no dropout) on training set
+                train_acc = self.accuracy.eval(
+                    feed_dict={self.x: self.x_train, self.y: self.y_train, self.keep_prob: 1.0}
+                )
+                print "epoch %d: train accuracy: %g" % (epoch, train_acc)
+                self.train_accuracies.append(train_acc)
+
+                # Evaluate (so no dropout) on validation set
+                valid_acc = self.accuracy.eval(
+                    feed_dict={self.x: self.x_valid, self.y: self.y_valid, self.keep_prob: 1.0}
+                )
+                print "epoch %d: valid accuracy: %g" % (epoch, valid_acc)
+                self.valid_accuracies.append(valid_acc)
+
+                # early stop & save
+                if valid_acc > best_valid_acc:
+                    best_valid_acc = valid_acc  # set best acc
+                    p = patience  # reset patience to initial value bcs score improved
+                    self.save()
+                else:
+                    p -= 1
+                print "epoch %d: patience: %d\n" % (epoch, p)
+                if p == 0:
+                    break
+
+    def save(self):
+        # TODO
+        # model1_dir = "./models/vote_estimator_model"
+        pass
+
+    def load(self):
+        # TODO
+        pass
 
 
 def main(args):
     # Load datasets
-    input_size, (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data('r', voted_only=True)
+    data = get_data(args.data, 'r', voted_only=True)
+    train, _, _ = data
 
     # parse arguments
     if args.activation_1 == 'swish':
@@ -235,61 +309,22 @@ def main(args):
         print "ERROR: unknown activation 1: %s" % args.activation_1
         return
 
-    # TODO: save model when early stop
-    # model1_dir = "./models/vote_estimator_model"
-    # TODO: use batch size that make sense according to data_size
-    # steps = len(y_train) / FLAGS.batch_size
-
-    predictions, cross_entropy, train_step, accuracy = build_model1(
-        input_size,
+    print "\nBuilding the network..."
+    estimator1 = ShortTermEstimator(
+        data,
         args.hidden_sizes_1,
         activation_1,
         tf.train.AdamOptimizer()
     )
 
-    train_accuracies = []
-    valid_accuracies = []
-    best_valid_acc = 0.0
-    patience = args.patience
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())  # initialize model
-        for epoch in range(20000):  # will probably stop before 20k epochs due to early stop
-            # do 1 epoch: go through all training_batches
-            for idx in range(0, len(y_train), args.batch_size):
-                _, loss = sess.run(
-                    [train_step, cross_entropy],
-                    feed_dict={x: x_train[idx: idx+args.batch_size],
-                               y: y_train[idx: idx+args.batch_size],
-                               keep_prob: 1.0 - args.dropout_rate_1}
-                )
-                step = idx / args.batch_size
-                if step % 10 == 0:
-                    print "epoch %d - step %d - training loss: %g" % (epoch, step, loss)
-            print "------------------------------"
-            # Evaluate (so no dropout) on training set
-            train_acc = accuracy.eval(
-                feed_dict={x: x_train, y: y_train, keep_prob: 1.0}
-            )
-            print "epoch %d: train accuracy: %g" % (epoch, train_acc)
-            train_accuracies.append(train_acc)
+    print "\nTraining the network..."
+    estimator1.train(
+        args.patience,
+        args.batch_size,
+        args.dropout_rate_1
+    )
+    print "done."
 
-            # Evaluate (so no dropout) on validation set
-            valid_acc = accuracy.eval(
-                feed_dict={x: x_valid, y: y_valid, keep_prob: 1.0}
-            )
-            print "epoch %d: valid accuracy: %g" % (epoch, train_acc)
-            valid_accuracies.append(valid_acc)
-
-            # early stop & save
-            if valid_acc > best_valid_acc:
-                bes_valid_acc = valid_acc # set best acc
-                patience = args.patience  # reset patience to initial value bcs score improved
-                # TODO: save current model!
-            else:
-                patience -= 1
-            print "epoch %d: patience: %d\n" % (epoch, patience)
-            if patience == 0:
-                break
 
 
 if __name__ == '__main__':
@@ -307,7 +342,7 @@ if __name__ == '__main__':
     parser.add_argument("-d1", "--dropout_rate_1", type=float, default=0.1, help="Probability of dropout layer in first network")
     parser.add_argument("-d2", "--dropout_rate_2", type=float, default=0.1, help="Probability of dropout layer in second network")
     args = parser.parse_args()
-    print args
+    print "\n", args
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % args.gpu
