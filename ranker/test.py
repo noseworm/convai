829e94071d8cceaa4fbba9e6d7a21a82649dd4d8
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-11-11 00:41:55 -0500

    support two graphs and two sessions at the same time

--- ranker/test.py
+++ ranker/test.py
@@ -11,143 +11,197 @@ import os
 
 from estimators import Estimator, ACTIVATIONS, OPTIMIZERS, SHORT_TERM_MODE, LONG_TERM_MODE
 
-MODE_TO_FLAGS = {
-    'short_term': SHORT_TERM_MODE,
-    'long_term': LONG_TERM_MODE
-}
 
-
-def main(args):
+def load_previous_model(prefix):
+    """
+    """
+    print "Loading model %s ..." % prefix
     # Load previously saved model arguments
-    print "Loading previous model arguments..."
-    with open("%sargs.pkl" % args.model, 'rb') as handle:
-        model_args = pkl.load(handle)
-
-    if len(model_args) == 12:
+    with open("%sargs.pkl" % prefix, 'rb') as handle:
         data, \
         hidden_dims, hidden_dims_extra, activation, \
         optimizer, learning_rate, \
         model_path, model_id, model_name, \
-        batch_size, dropout_rate, pretrained = model_args
-    elif len(model_args) == 8:
-        print "WARNING: %d model arguments. Reconstructing missing arguments."
-        data, \
-        hidden_dims, activation, \
-        optimizer, learning_rate, \
-        model_id, \
-        batch_size, dropout_rate = model_args
-        # reconstruct missing parameters
-        hidden_dims_extra = [hidden_dims[-1]]
-        model_name = args.model.split(model_id)[1].replace('_', '')
-        pretrained = None
-    else:
-        print "ERROR: %d model arguments." % len(model_args)
-        print "data + %s" % (model_args[1:],)
-        return
+        batch_size, dropout_rate, pretrained = pkl.load(handle)
 
     # reconstruct model_path just in case it has been moved:
-    model_path = args.model.split(model_id)[0]
+    model_path = prefix.split(model_id)[0]
     if model_path.endswith('/'):
         model_path = model_path[:-1]  # ignore the last '/'
 
     # Load previously saved model timings
-    with open("%stimings.pkl" % args.model, 'rb') as handle:
+    with open("%stimings.pkl" % prefix, 'rb') as handle:
         train_accuracies, valid_accuracies = pkl.load(handle)
 
-    n_folds = len(data[0])
-    # consider only the last `n_folds` accuracies! Accuracies before that may be from another objective
-    max_train = [max(train_accuracies[i]) for i in range(-n_folds, 0)]
-    max_valid = [max(valid_accuracies[i]) for i in range(-n_folds, 0)]
+    max_train = [max(accs) for accs in train_accuracies]
+    max_valid = [max(accs) for accs in valid_accuracies]
     print "prev. max train accuracies: %s" % (max_train,)
     print "prev. max valid accuracies: %s" % (max_valid,)
+    # consider only the last `n_folds` accuracies! Accuracies before that may be from another objective
+    n_folds = len(data[0])
+    max_train = max_train[-n_folds:]
+    max_valid = max_valid[-n_folds:]
     train_acc = np.mean(max_train)
     valid_acc = np.mean(max_valid)
     print "prev. best avg. train accuracy: %g" % train_acc
     print "prev. best avg. valid accuracy: %g" % valid_acc
 
-    print "Building the network..."
-    estimator = Estimator(
-        data,
-        hidden_dims, hidden_dims_extra, activation,
-        optimizer, learning_rate,
-        model_path, model_id, model_name
-    )
+    return data, \
+        hidden_dims, hidden_dims_extra, activation, \
+        optimizer, learning_rate, \
+        model_path, model_id, model_name, \
+        batch_size, dropout_rate, pretrained
 
-    with tf.Session() as sess:
-        print "Reset network parameters..."
-        estimator.load(sess, model_path, model_id, model_name)
-        print "Testing the network..."
-        test_acc = estimator.test(MODE_TO_FLAGS[args.mode])
-        print "test accuracy: %g" % test_acc
 
-        # print "\nContinue training the network..."
-        # estimator.train(
-        #     sess,
-        #     MODE_TO_FLAGS[args.mode],
-        #     args.patience,
-        #     batch_size,
-        #     dropout_rate,
-        #     save=False,
-        #     pretrained=(model_path, model_id, model_name),
-        #     previous_accuracies=(train_accuracies, valid_accuracies),
-        #     verbose=True
-        # )
-        # Consider the newly added accuracies!
-        # max_train = [max(estimator.train_accuracies[i]) for i in range(-n_folds, 0)]
-        # max_valid = [max(estimator.valid_accuracies[i]) for i in range(-n_folds, 0)]
-        # print "max train accuracies: %s" % (max_train,)
-        # print "max valid accuracies: %s" % (max_valid,)
-        # train_acc = np.mean(max_train)
-        # valid_acc = np.mean(max_valid)
-        # print "best avg. train accuracy: %g" % train_acc
-        # print "best avg. valid accuracy: %g" % valid_acc
-        # print "Re-testing the network..."
-        # test_acc = estimator.test(MODE_TO_FLAGS[args.mode])
-        # print "test accuracy: %g" % test_acc
+def build_graph(data, hidden_dims, hidden_dims_extra, activation, optimizer, learning_rate, model_path, model_id, model_name):
+    model_graph = tf.Graph()
+    with model_graph.as_default():
+        estimator = Estimator(
+            data,
+            hidden_dims, hidden_dims_extra, activation,
+            optimizer, learning_rate,
+            model_path, model_id, model_name
+        )
+    return model_graph, estimator
 
-        print "Get train, valid, test prediction..."
-        trains, valids, (x_test, y_test), feature_list = data
-        train_acc, valid_acc = [], []
-        for fold in range(len(trains)):
-            preds, confs = estimator.predict(MODE_TO_FLAGS[args.mode], trains[fold][0])
-            if args.mode == 'short_term':
+
+def main(args):
+    data_s, \
+        hidden_dims_s, hidden_dims_extra_s, activation_s, \
+        optimizer_s, learning_rate_s, \
+        model_path_s, model_id_s, model_name_s, \
+        batch_size_s, dropout_rate_s, pretrained_s = load_previous_model(args.short_term_model)
+
+    data_l, \
+        hidden_dims_l, hidden_dims_extra_l, activation_l, \
+        optimizer_l, learning_rate_l, \
+        model_path_l, model_id_l, model_name_l, \
+        batch_size_l, dropout_rate_l, pretrained_l = load_previous_model(args.long_term_model)
+
+
+    n_folds = len(data_s[0])
+    assert n_folds == len(data_l[0])
+
+    print "Building the networks..."
+    graph_s, estim_s = build_graph(data_s, hidden_dims_s, hidden_dims_extra_s, activation_s, optimizer_s, learning_rate_s, model_path_s, model_id_s, model_name_s)
+    graph_l, estim_l = build_graph(data_l, hidden_dims_l, hidden_dims_extra_l, activation_l, optimizer_l, learning_rate_l, model_path_l, model_id_l, model_name_l)
+
+    sess_s = tf.Session(graph=graph_s)
+    sess_l = tf.Session(graph=graph_l)
+
+    with sess_s.as_default():
+        with graph_s.as_default():
+            print "Reset short term network parameters..."
+            estim_s.load(sess_s, model_path_s, model_id_s, model_name_s)
+
+    with sess_l.as_default():
+        with graph_l.as_default():
+            print "Reset long term network parameters..."
+            estim_l.load(sess_l, model_path_l, model_id_l, model_name_l)
+
+    with sess_s.as_default():
+        with graph_s.as_default():
+            print "Testing the short term network..."
+            test_acc = estim_s.test(SHORT_TERM_MODE)
+            print "test accuracy: %g" % test_acc
+
+    with sess_l.as_default():
+        with graph_l.as_default():
+            print "Testing the long term network..."
+            test_acc = estim_l.test(LONG_TERM_MODE)
+            print "test accuracy OTHER: %g" % test_acc
+
+    '''Code below is to train again one of the model'''
+    # print "\nContinue training the network..."
+    # estimator.train(
+    #     sess,
+    #     MODE_TO_FLAGS[args.mode],
+    #     args.patience,
+    #     batch_size,
+    #     dropout_rate,
+    #     save=False,
+    #     pretrained=(model_path, model_id, model_name),
+    #     previous_accuracies=(train_accuracies, valid_accuracies),
+    #     verbose=True
+    # )
+    # Consider the newly added accuracies!
+    # max_train = [max(estimator.train_accuracies[i]) for i in range(-n_folds, 0)]
+    # max_valid = [max(estimator.valid_accuracies[i]) for i in range(-n_folds, 0)]
+    # print "max train accuracies: %s" % (max_train,)
+    # print "max valid accuracies: %s" % (max_valid,)
+    # train_acc = np.mean(max_train)
+    # valid_acc = np.mean(max_valid)
+    # print "best avg. train accuracy: %g" % train_acc
+    # print "best avg. valid accuracy: %g" % valid_acc
+    # print "Re-testing the network..."
+    # test_acc = estimator.test(MODE_TO_FLAGS[args.mode])
+    # print "test accuracy: %g" % test_acc
+
+    with sess_s.as_default():
+        with graph_s.as_default():
+            print "Get train, valid, test prediction..."
+            trains, valids, (x_test, y_test), feature_list = data_s
+            train_acc, valid_acc = [], []
+            for fold in range(len(trains)):
+                preds, confs = estim_s.predict(SHORT_TERM_MODE, trains[fold][0])
                 same = float(np.sum(preds == trains[fold][1]))
                 # TODO: compute true/false positives/negatives
-            else:
+                acc  = same/len(preds)
+                train_acc.append(acc)
+                print "[fold %d] train acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
+
+                preds, confs = estim_s.predict(SHORT_TERM_MODE, valids[fold][0])
+                same = float(np.sum(preds == valids[fold][1]))
+                # TODO: compute true/false positives/negatives
+                acc  = same/len(preds)
+                valid_acc.append(acc)
+                print "[fold %d] valid acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
+
+            print "avg. train acc. %g" % np.mean(train_acc)
+            print "avg. valid acc. %g" % np.mean(valid_acc)
+            preds, confs = estim_s.predict(SHORT_TERM_MODE, x_test)
+            same = float(np.sum(preds == y_test))
+            # TODO: compute true/false positives/negatives
+            acc = same/len(preds)
+            print "test acc: %g/%d=%g" % (same, len(preds), acc)
+
+
+    with sess_l.as_default():
+        with graph_l.as_default():
+            print "Get train, valid, test prediction..."
+            trains, valids, (x_test, y_test), feature_list = data_l
+            train_acc, valid_acc = [], []
+            for fold in range(len(trains)):
+                preds, confs = estim_l.predict(LONG_TERM_MODE, trains[fold][0])
                 same = -np.sum((preds - trains[fold][1])**2)
                 # TODO: plot x:labels y:predictions ~ confusion matrix/plot
-            acc  = same/len(preds)
-            train_acc.append(acc)
-            print "[fold %d] train acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
+                acc  = same/len(preds)
+                train_acc.append(acc)
+                print "[fold %d] train acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
 
-            preds, confs = estimator.predict(MODE_TO_FLAGS[args.mode], valids[fold][0])
-            if args.mode == 'short_term':
-                same = float(np.sum(preds == valids[fold][1]))
-            else:
+                preds, confs = estim_l.predict(LONG_TERM_MODE, valids[fold][0])
                 same = -np.sum((preds - valids[fold][1])**2)
-            acc  = same/len(preds)
-            valid_acc.append(acc)
-            print "[fold %d] valid acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
+                # TODO: plot x:labels y:predictions ~ confusion matrix/plot
+                acc  = same/len(preds)
+                valid_acc.append(acc)
+                print "[fold %d] valid acc: %g/%d=%g" % (fold+1, same, len(preds), acc)
 
-        print "avg. train acc. %g" % np.mean(train_acc)
-        print "avg. valid acc. %g" % np.mean(valid_acc)
-        preds, confs = estimator.predict(MODE_TO_FLAGS[args.mode], x_test)
-        if args.mode == 'short_term':
-            same = float(np.sum(preds == y_test))
-        else:
+            print "avg. train acc. %g" % np.mean(train_acc)
+            print "avg. valid acc. %g" % np.mean(valid_acc)
+            preds, confs = estim_l.predict(LONG_TERM_MODE, x_test)
             same = -np.sum((preds - y_test)**2)
-        acc = same/len(preds)
-        print "test acc: %g/%d=%g" % (same, len(preds), acc)
+            # TODO: plot x:labels y:predictions ~ confusion matrix/plot
+            acc = same/len(preds)
+            print "test acc: %g/%d=%g" % (same, len(preds), acc)
 
     print "done."
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("model", type=str, help="Model prefix to load")
-    parser.add_argument("mode", type=str, choices=['short_term', 'long_term'], help="test on SHORT TERM or LONG TERM accuracy")
+    parser.add_argument("short_term_model", type=str, help="Short term estimator prefix to load")
+    parser.add_argument("long_term_model", type=str, help="Long term estimator prefix to load")
     parser.add_argument("-g",  "--gpu", type=int, default=0, help="GPU number to use")
-    parser.add_argument("-p",  "--patience", type=int, default=5, help="Number of training steps to wait before stoping when validatiaon accuracy doesn't increase")
     args = parser.parse_args()
     print "\n%s\n" % args
 
--- ranker/test.sh
+++ ranker/test.sh
@@ -3,5 +3,8 @@
 ###
 # DEBUG and TEST script to get accuracy of a model on test set
 ###
-python test.py ./models/short_term/0.641391/1510248853.21_Estimator_ short_term --gpu 3 --patience 5
+python test.py \
+    ./models/short_term/0.641391/1510248853.21_Estimator_ \
+    ./models/long_term/1.4506/1510248853.21_short_term.0641391.151024885321_Estimator__ \
+    --gpu 3
 
