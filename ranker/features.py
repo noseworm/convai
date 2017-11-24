4c72b6b74896b3ef053e67e44228f755c88862b9
Koustuv Sinha <koustuv.sinha@mail.mcgill.ca>
2017-11-24 07:02:24 +0000

    changed logging level

--- ranker/features.py
+++ ranker/features.py
@@ -15,7 +15,7 @@ path = os.path.dirname(os.path.abspath(filename))
 import spacy
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s: %(name)s: %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(name)s: %(levelname)s: %(message)s")
 
 """
 This file defines different hand-enginered features based
